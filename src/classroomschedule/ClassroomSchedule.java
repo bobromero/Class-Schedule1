57588f089170625468385da86662f214421e6f4d
Robert Romero <21332@columbushs.com>
2019-08-30 09:38:40 -0400

    First Commit

---
+++ src/classroomschedule/ClassroomSchedule.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classroomschedule;
+
+/**
+ *
+ * @author 21332
+ */
+public class ClassroomSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String firstName = "Robert";
+        String lastName = " Romero";
+        int gradYear = 2021;
+        
+        System.out.println("Hello my name is " + firstName + lastName + " and I will graduate in " + gradYear + "!");
+        System.out.println("And this is my schedule");
+        System.out.println("+-------------------------------------------------------+");
+        System.out.println("| 1 | English III | Ms. Lapan | ");
+        System.out.println("| 2 | Precalculus | Mrs. Gideon |");
+        System.out.println("| 3 | Music Theory | Mr. Davis | ");
+        System.out.println("| 4 | Principles of Technology I | Ms. Garcia | ");
+        System.out.println("| 5 | Latin II | Mrs. Barnett | ");
+        System.out.println("| 6 | AP US History | Ms. Johannessen | ");
+        System.out.println("+-------------------------------------------------------+");
+    }
+    
+}
