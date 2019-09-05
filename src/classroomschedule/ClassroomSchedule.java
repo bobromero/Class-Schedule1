870a74c3f7bfd654be1391356d71cba5cac69861
Robert Romero <21332@columbushs.com>
2019-09-05 10:28:11 -0400

    henlo

--- src/classroomschedule/ClassroomSchedule.java
+++ src/classroomschedule/ClassroomSchedule.java
@@ -18,17 +18,39 @@ public class ClassroomSchedule {
         String firstName = "Robert";
         String lastName = " Romero";
         int gradYear = 2021;
+        String classOne = "Hon American History";
+        String teacherOne = "Sharifah Lee";
+        
+        String classTwo = "AP Computer Science A";
+        String teacherTwo = "Matthew Larrubia";
+        
+        String classThree = "Physics Accelerated";
+        String teacherThree = "Bruce Ferguson";
+        
+        String classFour = "English 11 Accelerated";
+        String teacherFour = "Andrew Chacon";
+        
+        String classFive = "Hon Printmaking";
+        String teacherFive = "Peter Santa-Maria";
+        
+        String classSix = "Hon Global Studies";
+        String teacherSix = "James Dugard";
+  
+        
+        String classSeven = "Algebra 2 Accelerated";
+        String teacherSeven = "Lissette Leonard";
         
         System.out.println("Hello my name is " + firstName + lastName + " and I will graduate in " + gradYear + "!");
         System.out.println("And this is my schedule");
-        System.out.println("+-------------------------------------------------------+");
-        System.out.println("| 1 | English III | Ms. Lapan | ");
-        System.out.println("| 2 | Precalculus | Mrs. Gideon |");
-        System.out.println("| 3 | Music Theory | Mr. Davis | ");
-        System.out.println("| 4 | Principles of Technology I | Ms. Garcia | ");
-        System.out.println("| 5 | Latin II | Mrs. Barnett | ");
-        System.out.println("| 6 | AP US History | Ms. Johannessen | ");
-        System.out.println("+-------------------------------------------------------+");
+        System.out.println("+--------------------------------------------+");
+        System.out.println("| 1 | " + classOne + " |     " + teacherOne +  "|");
+        System.out.println("| 2 |" + classTwo + " | " + teacherTwo + "| ");
+        System.out.println("| 3 |" + classThree + "   |   " + teacherThree + "| ");
+        System.out.println("| 4 |" + classFour + "|    " + teacherFour  +"| ");
+        System.out.println("| 5 |" + classFive + "       |" + teacherFive + "| ");
+        System.out.println("| 6 |" + classSix + "    |     " + teacherSix + "| ");
+        System.out.println("| 7 |" + classSeven + " | " + teacherSeven + "| ");
+        System.out.println("+--------------------------------------------+");
     }
     
 }
